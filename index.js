f8f1b649df97811afe3274a2d145ef127273a45d
Luis Martinez <luis_mmartinez@live.com>
2021-05-31 17:15:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+let num3 = 20
+let num4 = 8
+
+let multiply = num1 * num2;
+
+let random = Math.random() + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(num1, num4, num3, 3);
